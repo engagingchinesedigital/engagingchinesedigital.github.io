accf73f3afb585de84731dddd61fd224d84e95f3
jungxjung <jungxjung@gmail.com>
2022-07-26 11:23:34 +0800

    added shi's slide3

--- script/myjs.js
+++ script/myjs.js
@@ -3,7 +3,11 @@ document.getElementById("shibtn1").addEventListener("click", function() {
   });
 
 document.getElementById("shibtn2").addEventListener("click", function() {
-document.getElementById("shiframe2").setAttribute("src", "../file/Redesign the Speaking Class Shi .pdf");
+    document.getElementById("shiframe2").setAttribute("src", "../file/Redesign the Speaking Class Shi .pdf");
+});
+
+document.getElementById("shibtn3").addEventListener("click", function() {
+    document.getElementById("shiframe3").setAttribute("src", "../file/Final remarks Shi.pdf");
 });
 
 console.log("Hi");
