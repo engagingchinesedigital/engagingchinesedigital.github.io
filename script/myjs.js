0a589089e3dee5144af57f6171e9ae488f42c889
jungxjung <jungxjung@gmail.com>
2022-07-26 11:19:56 +0800

    javascript shi's first 2 slides

---
+++ script/myjs.js
@@ -0,0 +1,9 @@
+document.getElementById("shibtn1").addEventListener("click", function() {
+    document.getElementById("shiframe1").setAttribute("src", "../file/Shi.pdf");
+  });
+
+document.getElementById("shibtn2").addEventListener("click", function() {
+document.getElementById("shiframe2").setAttribute("src", "../file/Redesign the Speaking Class Shi .pdf");
+});
+
+console.log("Hi");
